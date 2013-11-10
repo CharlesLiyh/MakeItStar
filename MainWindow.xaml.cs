a160d1c592b32e51ea09f44abe52d1303606fea0
CharlesLiyh <charles.liyh@gmail.com>
2013-11-10 22:50:38 +0800

    Pre-codes added, with only star-making feature

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.IO;
+using System;
+
+namespace MakeItStar
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        protected void OnDrop(object sender, DragEventArgs e) {
+            string fileName = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            FileInfo fi = new FileInfo(fileName);
+
+            string star = "【★】";
+            string dstName;
+            if (fi.Name.StartsWith(star))
+            {
+                dstName = fi.Name.Substring(3);
+            }
+            else
+            {
+                dstName = star + fi.Name;
+            }
+
+            string dstPath = string.Format("{0}\\{1}", fi.DirectoryName, dstName);
+            if (File.Exists(dstPath))
+                File.Delete(dstPath);
+
+            fi.MoveTo(dstPath);
+        }
+    }
+}
